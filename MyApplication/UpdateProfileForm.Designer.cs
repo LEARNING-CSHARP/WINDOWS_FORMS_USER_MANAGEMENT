0d8dfe4cb690cd8c878f33e7a259907787b51ba8
Mr. Dariush Tasdighi <DariushT@GMail.com>
2017-02-17 09:46:07 +0330

    Adding UpdateProfile Designer to Source Control!

---
+++ MyApplication/UpdateProfileForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MyApplication
+{
+	partial class UpdateProfileForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.saveButton = new System.Windows.Forms.Button();
+			this.descriptionLabel = new System.Windows.Forms.Label();
+			this.descriptionTextBox = new System.Windows.Forms.TextBox();
+			this.fullNameTextBox = new System.Windows.Forms.TextBox();
+			this.fullNameLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// saveButton
+			// 
+			this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.saveButton.Location = new System.Drawing.Point(385, 288);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(87, 23);
+			this.saveButton.TabIndex = 4;
+			this.saveButton.Text = "&Save";
+			this.saveButton.UseVisualStyleBackColor = true;
+			this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// descriptionLabel
+			// 
+			this.descriptionLabel.AutoSize = true;
+			this.descriptionLabel.ForeColor = System.Drawing.Color.White;
+			this.descriptionLabel.Location = new System.Drawing.Point(14, 41);
+			this.descriptionLabel.Name = "descriptionLabel";
+			this.descriptionLabel.Size = new System.Drawing.Size(71, 13);
+			this.descriptionLabel.TabIndex = 2;
+			this.descriptionLabel.Text = "&Description";
+			// 
+			// descriptionTextBox
+			// 
+			this.descriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.descriptionTextBox.Location = new System.Drawing.Point(91, 38);
+			this.descriptionTextBox.Multiline = true;
+			this.descriptionTextBox.Name = "descriptionTextBox";
+			this.descriptionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.descriptionTextBox.Size = new System.Drawing.Size(381, 244);
+			this.descriptionTextBox.TabIndex = 3;
+			// 
+			// fullNameTextBox
+			// 
+			this.fullNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.fullNameTextBox.Location = new System.Drawing.Point(91, 12);
+			this.fullNameTextBox.Name = "fullNameTextBox";
+			this.fullNameTextBox.Size = new System.Drawing.Size(381, 21);
+			this.fullNameTextBox.TabIndex = 1;
+			// 
+			// fullNameLabel
+			// 
+			this.fullNameLabel.AutoSize = true;
+			this.fullNameLabel.ForeColor = System.Drawing.Color.White;
+			this.fullNameLabel.Location = new System.Drawing.Point(14, 15);
+			this.fullNameLabel.Name = "fullNameLabel";
+			this.fullNameLabel.Size = new System.Drawing.Size(63, 13);
+			this.fullNameLabel.TabIndex = 0;
+			this.fullNameLabel.Text = "&Full Name";
+			// 
+			// UpdateProfileForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(486, 323);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.descriptionLabel);
+			this.Controls.Add(this.descriptionTextBox);
+			this.Controls.Add(this.fullNameTextBox);
+			this.Controls.Add(this.fullNameLabel);
+			this.Name = "UpdateProfileForm";
+			this.Text = "Update Profile";
+			this.Load += new System.EventHandler(this.UpdateProfileForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label descriptionLabel;
+		private System.Windows.Forms.TextBox descriptionTextBox;
+		private System.Windows.Forms.TextBox fullNameTextBox;
+		private System.Windows.Forms.Label fullNameLabel;
+		private System.Windows.Forms.Button saveButton;
+	}
+}
